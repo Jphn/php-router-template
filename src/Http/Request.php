e56a37a9b831068b82bb4362c47d046793662c6b
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-06-12 11:27:22 -0300

    corrected request class comment

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,26 +6,26 @@ namespace App\Http;
 
 class Request
 {
-	// public readonly array $queryParams; // Works only on PHP ^8.0
-	// public readonly array $postVars; // Works only on PHP ^8.0
-	// public readonly array $headers; // Works only on PHP ^8.0
+	// public readonly array $queryParams; // Works only on PHP ^8.1
+	// public readonly array $postVars; // Works only on PHP ^8.1
+	// public readonly array $headers; // Works only on PHP ^8.1
 
-	private array $params; // Below 8.0
+	private array $params; // Below 8.1
 
 	public function __construct()
 	{
-		// $this->queryParams = $_GET; // Works only on PHP ^8.0
-		// $this->postVars = $_POST; // Works only on PHP ^8.0
-		// $this->headers = getallheaders(); // Works only on PHP ^8.0
+		// $this->queryParams = $_GET; // Works only on PHP ^8.1
+		// $this->postVars = $_POST; // Works only on PHP ^8.1
+		// $this->headers = getallheaders(); // Works only on PHP ^8.1
 
-		$this->params = [ // Below 8.0
+		$this->params = [ // Below 8.1
 			'queryParams' => $_GET ?? [],
 			'postVars' => $_POST ?? [],
 			'headers' => getallheaders()
 		];
 	}
 
-	public function __get($name) // Below 8.0
+	public function __get($name) // Below 8.1
 	{
 		return $this->params[$name] ?? null;
 	}
